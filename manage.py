024ae7f0a17dc26d87e913cb455b732aebe263f9
Robley <robleyadrian@gmail.com>
2018-08-08 01:19:41 +0300

    feat: add manage file.

---
+++ manage.py
@@ -0,0 +1,18 @@
+import os
+
+from flask_script import Manager # Class for handling a set of commands
+from flask_migrate import Migrate, MigrateCommand
+
+from app import db, create_app
+from app import models
+
+
+app = create_app(config_name=os.getenv('APP_ENV'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
