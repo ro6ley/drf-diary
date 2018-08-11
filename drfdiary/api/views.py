b305253b55cb60a6c939efc32922f6efeec3c582
Robley <robleyadrian@gmail.com>
2018-08-11 12:22:56 +0300

    feat: add new views

--- drfdiary/api/views.py
+++ drfdiary/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import EntrySerializer
+from rest_framework import permissions
+from django.contrib.auth import get_user_model
+
+from .permissions import IsOwner
+from .serializers import EntrySerializer, UserSerializer
 from .models import Entry
 
 
@@ -11,10 +15,28 @@ class CreateView(generics.ListCreateAPIView):
     """
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         """
         Save the POST data when creating a new entry
         """
-        serializer.save()
-        
+        serializer.save(owner=self.request.user)
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This class handles the http GET, PUT and DELETE requests.
+    """
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+
+class CreateUserView(generics.CreateAPIView):
+    """
+    Class to handle the registration of users in the API
+    """
+    model = get_user_model()
+    permission_classes = (permissions.AllowAny)
+    serializer_class = UserSerializer
