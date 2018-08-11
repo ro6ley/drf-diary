243b86e5cf3938f857d5efa1b363256e90b670c8
Robley <robleyadrian@gmail.com>
2018-08-11 12:22:26 +0300

    chore: update tests.

--- drfdiary/api/test_diary.py
+++ drfdiary/api/test_diary.py
@@ -1,7 +1,13 @@
+import json
+import ujson
+
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.urls import reverse
+
+from django.contrib.auth.models import User
+
 from .models import Entry
 
 
@@ -14,8 +20,9 @@ class ModelTestCase(TestCase):
         """
         Define the test client and other test variables
         """
+        user = User.objects.create(username="nerd")
         self.entry_content = "Dear Diary, I wrote tests today"
-        self.entry = Entry(content=self.entry_content)
+        self.entry = Entry(content=self.entry_content, owner=user)
 
     def test_model_can_create_an_entry(self):
         """
@@ -36,8 +43,12 @@ class ViewTestCase(TestCase):
         """
         Define the test client and other test variables
         """
+        user = User.objects.create(username="nerd")
+
+        # Initialize the client and force it to use auth
         self.client = APIClient()
-        self.entry_data = {"content": "Dear Diary, it's been so long"}
+        self.client.force_authenticate(user=user)
+        self.entry_data = {"content": "Dear Diary, it's been so long", "owner": user.id}
         self.response = self.client.post(
             reverse('create'),
             self.entry_data,
@@ -49,3 +60,22 @@ class ViewTestCase(TestCase):
         Test the API can create an entry
         """
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_authorization_is_enforced(self):
+        """
+        Test that the api has user authorization
+        """
+        new_client = APIClient()
+        res = new_client.get('/api/entries/', kwargs={'pk': 3}, format="json")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_api_can_get_an_entry(self):
+        """
+        Test the can get a given diary entry
+        """
+        entry = Entry.objects.get()
+        response = self.client.get(
+            reverse('details',
+                    kwargs={'pk': entry.id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
