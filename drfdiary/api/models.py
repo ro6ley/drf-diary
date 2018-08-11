11414b1952227155c985a05cf13028ad6bcbb0f3
Robley <robleyadrian@gmail.com>
2018-08-11 12:21:41 +0300

    feat: update models.

--- drfdiary/api/models.py
+++ drfdiary/api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -7,6 +11,9 @@ class Entry(models.Model):
     This class represents the entry model.
     """
     content = models.CharField(max_length=255, blank=False, unique=True)
+    owner = models.ForeignKey('auth.user', 
+                              related_name="entries", 
+                              on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -15,3 +22,10 @@ class Entry(models.Model):
         Return a string representation of the model instance
         """
         return "{} : {}".format(self.date_created, self.content)
+
+
+# This receiver handles token creation immediately a new user is created
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
