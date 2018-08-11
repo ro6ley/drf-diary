edae0cb0189a1b4489d06584b8b7ae9bc88205df
Robley <robleyadrian@gmail.com>
2018-08-11 12:39:12 +0300

    chore: update urls.

--- drfdiary/api/urls.py
+++ drfdiary/api/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken.views import obtain_auth_token
+import rest_auth
 
 from .views import CreateView, DetailsView
 
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^entries/$', CreateView.as_view(), name="create"),
     url(r'^entries/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
     url(r'^get-token/$', obtain_auth_token)
