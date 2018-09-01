227eed734e82ed3ed3611ab4a7c560bd2f3ebc42
Robley <robleyadrian@gmail.com>
2018-09-01 20:06:08 +0300

    feat: add url for documentation.

--- drfdiary/urls.py
+++ drfdiary/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('docs/', include_docs_urls(title='Diary API Documentation', public=True)),
 ]
