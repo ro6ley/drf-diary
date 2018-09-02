27c898a2f22bf5e794a6cb7f64c50f4b59df3660
Robley <robleyadrian@gmail.com>
2018-09-02 22:59:02 +0300

    chore: refactor tests and add test case.

--- drfdiary/api/tests/test_diary.py
+++ drfdiary/api/tests/test_diary.py
@@ -42,17 +42,27 @@ class ViewTestCase(TestCase):
         """
         Define the test client and other test variables
         """
-        user = User.objects.create(username="nerd")
+        self.user = User.objects.create(username="nerd", password="password")
 
         # Initialize the client and force it to use auth
         self.client = APIClient()
-        self.client.force_authenticate(user=user)
-        self.entry_data = {"content": "Dear Diary, it's been so long", "owner": user.id}
-        self.response = self.client.post(
-            reverse('create'),
-            self.entry_data,
-            format="json"
-        )
+        self.client.force_authenticate(user=self.user)
+        self.entry_data = {"content": "Dear Diary, it's been so long", "owner": self.user.id}
+        self.response = self.client.post('/api/v1/entries/', self.entry_data, format="json")
+    
+    def test_can_create_a_user(self):
+        """
+        Test user registration via the API
+        """
+        response = self.client.post('/api/v1/rest-auth/registration/', 
+                                    {
+                                        "username": "robley",
+                                        "password1": "0@qwer687",
+                                        "password2": "0@qwer687",
+                                        "email": "random@guy.com"
+                                    },
+                                    format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_api_can_create_an_entry(self):
         """
@@ -60,12 +70,19 @@ class ViewTestCase(TestCase):
         """
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_api_can_list_all_users(self):
+        """
+        Test that the API can return a list of all users
+        """
+        response = self.client.get('/api/v1/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_authorization_is_enforced(self):
         """
         Test that the api has user authorization
         """
         new_client = APIClient()
-        res = new_client.get('/api/entries/', kwargs={'pk': 3}, format="json")
+        res = new_client.get('/api/v1/entries/', kwargs={'pk': 3}, format="json")
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_api_can_get_an_entry(self):
@@ -73,8 +90,15 @@ class ViewTestCase(TestCase):
         Test the can get a given diary entry
         """
         entry = Entry.objects.get()
-        response = self.client.get(
-            reverse('details',
-                    kwargs={'pk': entry.id}), format="json"
-        )
+        url = "/api/v1/entries/{}/".format(entry.id)
+        response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_an_entry(self):
+        """
+        Test the deletion of a given diary entry
+        """
+        entry = Entry.objects.get()
+        url = "/api/v1/entries/{}/".format(entry.id)
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
