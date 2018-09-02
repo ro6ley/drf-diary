3b7504671161c065340fb66a1e9ed0fd538934bb
Robley <robleyadrian@gmail.com>
2018-09-02 23:47:52 +0300

    chore: refactor permissions.

--- drfdiary/api/permissions.py
+++ drfdiary/api/permissions.py
@@ -4,13 +4,13 @@ from .models import Entry
 
 class IsOwner(permissions.BasePermission):
     """
-    Custom permission class to allow only bucketlist owners to edit them.
+    Custom permission class to allow only entry owners to edit them.
     """
 
 
     def has_object_permission(self, request, view, obj):
         """
-        Return True id permission is granted to the bucketlist owner
+        Return True id permission is granted to the entry owner
         """
         if isinstance(obj, Entry):
             return obj.owner == request.user
