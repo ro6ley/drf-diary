ac6f6fcee833159a5f496b92f17dc8c6f72d4e93
Robley <robleyadrian@gmail.com>
2018-09-02 23:27:13 +0300

    feat: add more tests.

--- drfdiary/api/tests/test_diary.py
+++ drfdiary/api/tests/test_diary.py
@@ -70,6 +70,14 @@ class ViewTestCase(TestCase):
         """
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_api_cannot_create_without_auth(self):
+        """
+        Test that creation of an entry requires authentication
+        """
+        client = APIClient()
+        response = client.post('/api/v1/entries/', {"content": "I cannot be created"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_api_can_list_all_users(self):
         """
         Test that the API can return a list of all users
@@ -77,13 +85,13 @@ class ViewTestCase(TestCase):
         response = self.client.get('/api/v1/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_authorization_is_enforced(self):
+    def test_api_cannot_list_all_users_without_auth(self):
         """
-        Test that the api has user authorization
+        Test that authentication is required to list all users
         """
-        new_client = APIClient()
-        res = new_client.get('/api/v1/entries/', kwargs={'pk': 3}, format="json")
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        client = APIClient()
+        response = client.get('/api/v1/users/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_api_can_get_an_entry(self):
         """
@@ -93,6 +101,48 @@ class ViewTestCase(TestCase):
         url = "/api/v1/entries/{}/".format(entry.id)
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_cannot_get_entry_without_auth(self):
+        """
+        Test that authentication is required to fetch an entry
+        """
+        response_content = json.loads(self.response.content.decode("utf-8"))
+        client = APIClient()
+        response = client.get('/api/v1/entries/{}/'.format(response_content["id"]))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_api_can_update_an_entry(self):
+        """
+        Test that we can update an entry
+        """
+        entry = Entry.objects.get()
+        url = "/api/v1/entries/{}/".format(entry.id)
+        response = self.client.put(url, {"content": "I am going to be updated"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_cannot_update_entry_without_auth(self):
+        """
+        Test that auth is needed to update an entry
+        """
+        client = APIClient()
+        entry = Entry.objects.get()
+        url = "/api/v1/entries/{}/".format(entry.id)
+        response = client.put(url, {"content": "I am going to be updated"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cannot_view_another_user_entry(self):
+        """
+        Test that a user cannot view another user's entries
+        """
+        client = APIClient()
+        # create a new user
+        new_user = User.objects.create(username="another_nerd")
+        # log them is
+        self.client.force_authenticate(user=new_user)
+        # get the entry we created with the user in the setup method
+        response_content = json.loads(self.response.content.decode("utf-8"))
+        response = client.get('/api/v1/entries/{}/'.format(response_content["id"]))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_api_can_delete_an_entry(self):
         """
