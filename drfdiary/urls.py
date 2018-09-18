97b6731678a9cbc41d4ef0c4ac0624f306700106
Robley <robleyadrian@gmail.com>
2018-09-19 00:54:51 +0300

    feat: update root url redirect.

--- drfdiary/urls.py
+++ drfdiary/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('docs/', include_docs_urls(title='Diary API Documentation',
                                     public=True)),
-    re_path(r'^/$', RedirectView.as_view(url='/docs/')),
+    re_path(r'^$', RedirectView.as_view(url='/docs/')),
 ]
