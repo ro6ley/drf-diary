bdea66eba897d742bafd383269913e32c55b5116
Robley <robleyadrian@gmail.com>
2018-09-18 13:23:51 +0300

    feat: add url for the categories view.

--- drfdiary/api/urls.py
+++ drfdiary/api/urls.py
@@ -3,12 +3,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken.views import obtain_auth_token
 import rest_auth
 
-from .views import CreateView, DetailsView, UserDetailsView, UserView
+from .views import CreateView, DetailsView, UserDetailsView, UserView, CategoryView
 
 
 urlpatterns = {
     url(r'^accounts/', include('rest_auth.urls')),
     url(r'^accounts/registration/', include('rest_auth.registration.urls')),
+    url(r'^categories/', CategoryView.as_view(), name="category_view"),
     url(r'^entries/$', CreateView.as_view(), name="create"),
     url(r'^entries/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
     url(r'^get-token/$', obtain_auth_token),
